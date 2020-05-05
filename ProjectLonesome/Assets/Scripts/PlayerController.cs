79b7de45c6e293fc3cc5d7ff36fff5de389c27d1
Mikael <mikaelkorpela31@gmail.com>
2020-05-05 17:25:58 +0300

    Created movement scripts
    Created a placeholder level and player character to work out movement scripts and interactions.

---
+++ ProjectLonesome/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(PlayerMotor))]
+public class PlayerController : MonoBehaviour
+{
+    public LayerMask movementMask;
+
+    Camera cam;
+    PlayerMotor motor;
+
+    void Start()
+    {
+        cam = Camera.main;
+        motor = GetComponent<PlayerMotor>();
+    }
+
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))    // Check for left-click mouse button
+        {
+            Ray ray = cam.ScreenPointToRay(Input.mousePosition);
+            RaycastHit hit;
+
+            if (Physics.Raycast(ray, out hit, 100, movementMask))
+            {
+                //Debug.Log("Hit " + hit.collider.name + " " + hit.point);
+                motor.MoveToPoint(hit.point);
+
+                // Stop focusing any objects
+
+            }
+        }
+
+        if (Input.GetMouseButtonDown(1))    // Check for right-click mouse button
+        {
+            Ray ray = cam.ScreenPointToRay(Input.mousePosition);
+            RaycastHit hit;
+
+            if (Physics.Raycast(ray, out hit, 100))
+            {
+                // Check if we hit an interactable
+                // Set focus
+
+            }
+        }
+    }
+}
