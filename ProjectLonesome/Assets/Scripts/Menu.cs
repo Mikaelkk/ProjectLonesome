8a52de123972763e758b69cdfec7c006972cc8e8
Mikael <mikaelkorpela31@gmail.com>
2020-05-09 23:25:55 +0300

    Menu/Credits set up
    Most of the core mechanics have been completed. Also finished up a simple menu / credits scene (+ in game menu). Should all be functional now.

---
+++ ProjectLonesome/Assets/Scripts/Menu.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Menu : MonoBehaviour
+{
+
+    public GameObject PauseMenu;
+    bool paused = false;
+
+    void Update()
+    {
+        if (Input.GetButtonDown("Cancel"))
+        {
+            paused = togglePause();
+        }
+    }
+
+    bool togglePause()
+    {
+        if (Time.timeScale == 0f)
+        {
+            Time.timeScale = 1f;
+            PauseMenu.SetActive(false);
+            return (false);
+        }
+        else
+        {
+            Time.timeScale = 0f;
+            PauseMenu.SetActive(true);
+            return (true);
+        }
+    }
+
+
+    public void StartGame()
+    {
+        // paused = togglePause();
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+
+    public void BackToMenu()
+    {
+        paused = togglePause();
+        SceneManager.LoadScene(0);
+    }
+
+    public void Quit()
+    {
+        // paused = togglePause();
+        Debug.Log("Quit game.");
+        Application.Quit();
+    }
+}
